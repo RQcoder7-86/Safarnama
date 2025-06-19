b35c3954dd720cf87ccb6717cf10a5c0738714e6
RQcoder <RQcoder7-86@users.noreply.github.com>
2025-06-19 18:17:28 +0500

    💡 Updated booking page to reload after submission

--- routes/booking.js
+++ routes/booking.js
@@ -1,7 +1,10 @@
-const express = require("express")
-const { v4: uuidv4 } = require('uuid');
-const router =express.Router();
-router.get("/",(req,res)=>{
-    res.render("pages/booking")
-})
-module.exports = router
+const express = require("express");
+const router = express.Router();
+
+
+router.get("/", (req, res) => {
+  res.render("pages/booking");
+});
+
+
+module.exports = router;
